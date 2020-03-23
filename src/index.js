84a9e4948904c869e3f98aa736c44ab087101c5f
Matt Kilcup <nevermindthelabel@gmail.com>
2020-03-22 22:08:03 -0700

    added an additional class

--- src/index.js
+++ src/index.js
@@ -5,33 +5,45 @@ class ListItem extends LitElement {
   constructor() {
     super();
     this.name = 'Hipsum';
-    this.description = 'Dummy text';
   }
 
   static get properties() {
     return {
       name: { type: String },
+    }
+  }
+
+  render() {
+    return html`
+    <div className="list" @click="${this.clickEvent}">
+      <h2>${this.name}</h2>
+      <!-- <p>${this.description}</p> -->
+      <p><list-item-description></list-item-description></p>
+    </div>
+    `;
+  }
+}
+
+
+customElements.define('list-item', ListItem);
+
+class ListItemDescription extends LitElement {
+  constructor() {
+    super();
+    // this.description = this.description;
+  }
+
+  static get properties() {
+    return {
       description: { type: String }
     }
   }
 
   render() {
     return html`
-     <style>
-      .list {
-        margin: 10px;
-        padding: 10px;
-        border: solid 1px black;
-        border-radius: 10px;
-        min-height: 50px;
-      }
-    </style>
-    <div className="list">
-      <h2>${this.name}</h2>
-      <p>${this.description}</p>
-    </div>
-    `;
-  }
-}
+    <p>Test</p>
+  `;
+  };
+};
 
-customElements.define('list-item', ListItem);
+customElements.define('list-item-description', ListItemDescription);
